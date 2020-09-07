bfdf0e06036657c190c0c5c7f2f0bb645c44a3b7
“always” <740905172@qq.com>
2020-09-07 15:40:48 +0800

    修改商品详情页面底部操作栏

--- src/pages/ProductDetail/components/BottomOperatingArea/incex.tsx
+++ src/pages/ProductDetail/components/BottomOperatingArea/incex.tsx
@@ -1,44 +1,49 @@
-import Taro from "@tarojs/taro";
-import React from "react";
-import { View, Text } from "@tarojs/components";
-import styles from "./index.module.scss";
-import { AtIcon } from "taro-ui";
-import { IBottomOperatingArea } from "./interface";
-import { setClassName } from "../../../../utils";
+import Taro from '@tarojs/taro';
+import { AtButton } from 'taro-ui';
+import React from 'react';
+import { View, Text } from '@tarojs/components';
+import styles from './index.module.scss';
+import { AtIcon } from 'taro-ui';
+import { IBottomOperatingArea } from './interface';
+import { setClassName } from '../../../../utils';
 
 const BottomOperatingArea = ({ favorites = false }: IBottomOperatingArea) => {
   return (
-    <View className={styles["operating-area-wrapper"]}>
-      <View className={styles["operating-area-con"]}>
-        <View className={styles["operating-area-item"]}>
-          <AtIcon prefixClass="icon" className={styles["icon"]} value="kefu" />
-          <Text className={styles["label"]}>客服</Text>
+    <View className={styles['operating-area-wrapper']}>
+      <View className={styles['operating-area-con']}>
+        <View className={styles['operating-area-item']}>
+          <AtIcon prefixClass="icon" className={styles['icon']} value="kefu" />
+          <Text className={styles['label']}>客服</Text>
         </View>
-        <View className={styles["operating-area-item"]}>
+        <View className={styles['operating-area-item']}>
           <AtIcon
             prefixClass="icon"
-            className={setClassName(["iconfont", styles["icon"]])}
+            className={setClassName(['iconfont', styles['icon']])}
             value="shoucang1"
-            color={`${favorites ? "#e93b3d" : ""}`}
+            color={`${favorites ? '#e93b3d' : ''}`}
           />
-          <Text className={styles["label"]}>收藏</Text>
+          <Text className={styles['label']}>收藏</Text>
         </View>
-        <View
+        <AtButton
+          type="primary"
+          size="small"
           className={setClassName([
-            styles["operating-area-item"],
-            styles["btn"],
+            styles['operating-area-item'],
+            styles['btn'],
           ])}
         >
-          <Text className={styles["label"]}>加入购物车</Text>
-        </View>
-        <View
+          加入购物车
+        </AtButton>
+        <AtButton
+          type="primary"
+          size="small"
           className={setClassName([
-            styles["operating-area-item"],
-            styles["btn"],
+            styles['operating-area-item'],
+            styles['btn'],
           ])}
         >
-          <Text className={styles["label"]}>立即购买</Text>
-        </View>
+          立即购买
+        </AtButton>
       </View>
     </View>
   );
