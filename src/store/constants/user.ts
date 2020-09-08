88b189e092817daf10b484c3a72d332d84bf91fb
“always” <740905172@qq.com>
2020-09-08 14:54:38 +0800

    商品详情底部操作栏兼容异形屏

---
+++ src/store/constants/user.ts
@@ -0,0 +1,8 @@
+/*
+ * @Author: Always
+ * @LastEditors: Always
+ * @Date: 2020-09-08 14:23:43
+ * @LastEditTime: 2020-09-08 14:23:47
+ * @FilePath: /koala-frontend/src/store/constants/user.ts
+ */
+export const UPDATE_USER_INFO = 'UPDATE_USER_INFO';
